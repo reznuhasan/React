2e8a2a438c3174819c8620351baf92b9ed48ecee
reznuhasan <reznuhasan31@gmail.com>
2023-04-10 13:20:03 +0600

    add first app

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -4,32 +4,33 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+ const friends=[
+  {name:'Ahmed',age:40,habit:'sleeping'},
+  {name:'Mohammed',age:35,habit:"Learning"},
+  {name:"Rishu",age:21,habit:"Coding"},
+  {name:"Rizwan",age:24,habit:"Programming"}
+ ]
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    {
+      friends.map(ch=><Person ch={ch}></Person>)
+    }
     </div>
   )
 }
-
+function Person(props){
+  const {name,age,habit}=props.ch
+  return <div style={{
+    border:'2px solid red',
+    padding:'20px',
+    backgroundColor:'whitesmoke',
+    color:'blue',
+    margin:'10px',
+    borderRadius:'5px'
+  }}>
+    <h1>Name:{name}</h1>
+    <h3>Age:{age}</h3>
+    <h3>Habit:{habit}</h3>
+  </div>
+}
 export default App
