447cc24e2673131a6008dcef4d389f79aac94175
reznuhasan <reznuhasan31@gmail.com>
2023-08-29 04:49:48 +0600

    setup is done

---
+++ Namastea React/MyShop/src/App.js
@@ -0,0 +1,12 @@
+import ReactDOM from "react-dom/client"
+const App=()=>{
+    return (
+        <div>
+            <h1>Hello React</h1>
+        </div>
+    )
+}
+
+const root=ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(<App/>)
