13e38a7b09838ab07037453605ddcb7d1379949d
reznuhasan <reznuhasan31@gmail.com>
2023-09-05 08:54:16 +0600

    studentinfo is finish

--- Student-Management-Project/src/components/Admission/StudentInfo.js
+++ Student-Management-Project/src/components/Admission/StudentInfo.js
@@ -1,8 +1,8 @@
-
-
 import { useFormik } from 'formik';
 import React from 'react'
 import "../../styles/StudentInfo.css"
+import { personalSchema } from '../../Model/PersonalSchema';
+import { useNavigate } from 'react-router-dom';
 const initialValues = {
   firstName: "",
   lastName: "",
@@ -13,18 +13,24 @@ const initialValues = {
   placeOfBirth: "",
   gender: "",
   maritalStatus: "",
-  permanentAddress: "",
   presentAddress: "",
+  permanentAddress: "",
   nationality: "",
   religion: "",
   nationalId: "",
   passportNo: "",
 }
+
 const StudentInfo = () => {
+  const navigate=useNavigate()
   const formik = useFormik({
-    initialValues,
+    initialValues:initialValues,
+    validationSchema:personalSchema,
     onSubmit: (values, action) => {
       console.log(values);
+      if(formik.isValid){
+        navigate('/')
+      }
       action.resetForm()
     },
   })
@@ -40,7 +46,9 @@ const StudentInfo = () => {
             type="text"
             onChange={formik.handleChange}
             value={formik.values.firstName}
+            onBlur={formik.handleBlur}
           />
+          {formik.errors.firstName && formik.touched.firstName?<p className='form-error'>{formik.errors.firstName}</p>:""}
         </div>
         <div className='inputField'>
           <label htmlFor='lastName'>Last Name*</label>
@@ -49,8 +57,10 @@ const StudentInfo = () => {
             name='lastName'
             type='text'
             onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
             value={formik.values.lastName}
           />
+          {formik.errors.lastName && formik.touched.lastName?<p className='form-error'>{formik.errors.lastName}</p>:""}
         </div>
         <div className='inputField'>
           <label htmlFor='nickName'>Nick Name</label>
@@ -60,7 +70,9 @@ const StudentInfo = () => {
             type='text'
             onChange={formik.handleChange}
             value={formik.values.nickName}
+            onBlur={formik.handleBlur}
           />
+          {formik.errors.nickName && formik.touched.nickName?<p className='form-error'>{formik.errors.nickName}</p>:""}
         </div>
         <div className='inputField'>
           <label htmlFor='phone'>Phone No*</label>
@@ -70,7 +82,9 @@ const StudentInfo = () => {
             type='text'
             onChange={formik.handleChange}
             value={formik.values.phone}
+            onBlur={formik.handleBlur}
           />
+          {formik.errors.phone && formik.touched.phone?<p className='form-error'>{formik.errors.phone}</p>:""}
         </div>
         <div className='inputField'>
           <label htmlFor='email'>Email*</label>
@@ -80,7 +94,9 @@ const StudentInfo = () => {
             type='email'
             onChange={formik.handleChange}
             value={formik.values.email}
+            onBlur={formik.handleBlur}
           />
+          {formik.errors.email && formik.touched.email?<p className='form-error'>{formik.errors.email}</p>:""}
         </div>
         <div className='inputField'>
           <label htmlFor='dateOfBirth'>Date Of Birth*</label>
@@ -90,7 +106,9 @@ const StudentInfo = () => {
             type='date'
             onChange={formik.handleChange}
             value={formik.values.dateOfBirth}
+            onBlur={formik.handleBlur}
           />
+          {formik.errors.dateOfBirth && formik.touched.dateOfBirth?<p className='form-error'>{formik.errors.dateOfBirth}</p>:""}
         </div>
         <div className='inputField'>
           <label htmlFor='placeOfBirth'>Place Of Birth*</label>
@@ -100,7 +118,9 @@ const StudentInfo = () => {
             type='text'
             onChange={formik.handleChange}
             value={formik.values.placeOfBirth}
+            onBlur={formik.handleBlur}
           />
+          {formik.errors.placeOfBirth && formik.touched.placeOfBirth?<p className='form-error'>{formik.errors.placeOfBirth}</p>:""}
         </div>
         <div className='personal-info-selection'>
           <div>
@@ -111,12 +131,14 @@ const StudentInfo = () => {
               type='radio'
               onChange={formik.handleChange}
               value={formik.values.gender}
+              onBlur={formik.handleBlur}
             >
               <option value="">Select Gender</option>
               <option value="male">Male</option>
               <option value="female">Female</option>
               <option value="other">Other</option>
             </select>
+            {formik.errors.gender && formik.touched.gender?<p className='form-error'>{formik.errors.gender}</p>:""}
           </div>
           <div>
             <label htmlFor='maritalStatus'>Marital Status*</label>
@@ -126,11 +148,13 @@ const StudentInfo = () => {
               type="radio"
               onChange={formik.handleChange}
               value={formik.values.maritalStatus}
+              onBlur={formik.handleBlur}
             >
               <option value="">Select Marital Status</option>
               <option value="married">Married</option>
               <option value="single">Single</option>
             </select>
+            {formik.errors.maritalStatus && formik.touched.maritalStatus?<p className='form-error'>{formik.errors.maritalStatus}</p>:""}
           </div>
         </div>
         <div className='inputField'>
@@ -141,7 +165,9 @@ const StudentInfo = () => {
             name="presentAddress"
             onChange={formik.handleChange}
             value={formik.values.presentAddress}
+            onBlur={formik.handleBlur}
           />
+          {formik.errors.presentAddress && formik.touched.presentAddress?<p className='form-error'>{formik.errors.presentAddress}</p>:""}
         </div>
         <div className='inputField'>
           <label htmlFor="permanentAddress">Permanent Address*:</label>
@@ -151,7 +177,9 @@ const StudentInfo = () => {
             name="permanentAddress"
             onChange={formik.handleChange}
             value={formik.values.permanentAddress}
+            onBlur={formik.handleBlur}
           />
+          {formik.errors.permanentAddress && formik.touched.permanentAddress?<p className='form-error'>{formik.errors.permanentAddress}</p>:""}
         </div>
         
         <div className='inputField'>
@@ -162,7 +190,9 @@ const StudentInfo = () => {
             name="nationality"
             onChange={formik.handleChange}
             value={formik.values.nationality}
+            onBlur={formik.handleBlur}
           />
+          {formik.errors.nationality && formik.touched.nationality?<p className='form-error'>{formik.errors.nationality}</p>:""}
         </div>
         <div className='inputField'>
           <label htmlFor="religion">Religion*:</label>
@@ -172,7 +202,9 @@ const StudentInfo = () => {
             name="religion"
             onChange={formik.handleChange}
             value={formik.values.religion}
+            onBlur={formik.handleBlur}
           />
+          {formik.errors.religion && formik.touched.religion?<p className='form-error'>{formik.errors.religion}</p>:""}
         </div>
         <div className='inputField'>
           <label htmlFor="nationalId">National Id*:</label>
@@ -182,7 +214,9 @@ const StudentInfo = () => {
             name="nationalId"
             onChange={formik.handleChange}
             value={formik.values.nationalId}
+            onBlur={formik.handleBlur}
           />
+          {formik.errors.nationalId && formik.touched.nationalId?<p className='form-error'>{formik.errors.nationalId}</p>:""}
         </div>
         <div className='inputField'>
           <label htmlFor="passportNo">Passport No:</label>
