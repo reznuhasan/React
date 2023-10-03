27c04ec9e8aecec7e96cbd594228529434b08895
reznuhasan <reznuhasan31@gmail.com>
2023-10-04 03:14:49 +0600

    header part complete

--- hospital-management-project/src/components/Header.jsx
+++ hospital-management-project/src/components/Header.jsx
@@ -2,24 +2,31 @@ import React from 'react'
 import logo from "../assets/smileLogo.png"
 import Styles from "../Styles/Header.module.css"
 import { NavLink } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+
 const Header = () => {
-  return (
-    <div>
-        <div className={Styles.logo}>
-            <img src={logo} alt="" />
-            <h1>EverSmile</h1>
+    return (
+        <div className={Styles.container}>
+            <div className={Styles.header}>
+                <div className={Styles.logo}>
+                    <img src={logo} alt="" />
+                    <h1>EverSmile</h1>
+                </div>
+                <div className={Styles.menu}>
+                    <NavLink to="/">Home</NavLink>
+                    <NavLink to="/about">About Us</NavLink>
+                    <NavLink to="/doctors">Find Doctor</NavLink>
+                    <NavLink to="/cabin">Cabins</NavLink>
+                    <NavLink to="/test">Tests</NavLink>
+                    <NavLink to="/nurses">Find Nurses</NavLink>
+                </div>
+            </div>
+            <div className={Styles.auth}>
+                <FontAwesomeIcon icon={faUser} />
+            </div>
         </div>
-        <div className="menu">
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/about">About Us</NavLink>
-            <NavLink to="/doctors">Find Doctor</NavLink>
-            <NavLink to="/cabin">Cabins</NavLink>
-            <NavLink to="/test">tests</NavLink>
-            <NavLink to="/nurses">Find Nurses</NavLink>
-            
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Header
