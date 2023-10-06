2b1b18c7ef77ce93384006d2ecb4b430d5623d26
reznuhasan <reznuhasan31@gmail.com>
2023-10-07 01:13:01 +0600

    footer part finish

--- hospital-management-project/src/components/Footer.jsx
+++ hospital-management-project/src/components/Footer.jsx
@@ -4,7 +4,7 @@ import logo from "../assets/smileLogo.png"
 import { NavLink } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPhone } from '@fortawesome/free-solid-svg-icons'
-import { FaFacebook, FaLinkedinIn, FaSquareInstagram, FaXTwitter, FaYoutube } from "react-icons/fa6";
+import { FaFacebookF, FaLinkedinIn, FaSquareInstagram, FaXTwitter, FaYoutube } from "react-icons/fa6";
 
 
 const Footer = () => {
@@ -14,34 +14,46 @@ const Footer = () => {
                 <img src={logo} alt="" />
                 <h1>EverSmile</h1>
             </div>
-            <div className={Styles.footerDetails}>
-                <div className={Styles.menu}>
-                    <NavLink to="/about">About Us</NavLink>
-                    <NavLink to="/doctors">Find Doctor</NavLink>
-                    <NavLink to="/cabin">Cabins</NavLink>
-                    <NavLink to="/test">Tests</NavLink>
-                    <NavLink to="/magazine">Magazine</NavLink>
-                    <NavLink to="/nurses">Find Nurses</NavLink>
-                </div>
-                <div className={Styles.contact}>
-                    <div className={Styles.inqury}>
-                        <p>Send Inqury</p>
+            <div className={Styles.allDetails}>
+                <div className={Styles.footerDetails}>
+                    <div className={Styles.menu}>
+                        <NavLink to="/about">About Us</NavLink>
+                        <NavLink to="/doctors">Find Doctor</NavLink>
+                        <NavLink to="/cabin">Cabins</NavLink>
+                        <NavLink to="/test">Tests</NavLink>
+                        <NavLink to="/magazine">Magazine</NavLink>
+                        <NavLink to="/nurses">Find Nurses</NavLink>
                     </div>
-                    <div className={Styles.hotline}>
-                        <FontAwesomeIcon icon={faPhone} className={Styles.icon}></FontAwesomeIcon><p>10678</p>
+                    <div className={Styles.contact}>
+                        <div className={Styles.inqury}>
+                            <p>Send Inqury</p>
+                        </div>
+                        <div className={Styles.hotline}>
+                            <FontAwesomeIcon icon={faPhone} className={Styles.icon}></FontAwesomeIcon><p>10678</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className={Styles.line}>
+                <div className={Styles.line}>
 
-            </div>
-            <div className={Styles.socialMedia}>
-                <FaFacebook/>
-                <FaSquareInstagram/>
-                <FaXTwitter/>
-                <FaYoutube/>
-                <FaLinkedinIn/>
-            {/* <FontAwesomeIcon icon={faFa} /> */}
+                </div>
+                <div className={Styles.socialMedia}>
+                    <div className={Styles.socialBox}>
+                        <FaFacebookF className={Styles.socialIcon} />
+                    </div>
+                    <div className={Styles.socialBox}>
+                        <FaSquareInstagram className={Styles.socialIcon} />
+                    </div>
+                    <div className={Styles.socialBox}>
+                        <FaXTwitter className={Styles.socialIcon} />
+                    </div>
+                    <div className={Styles.socialBox}>
+                        <FaYoutube className={Styles.socialIcon} />
+                    </div>
+                    <div className={Styles.socialBox}>
+                        <FaLinkedinIn className={Styles.socialIcon} />
+                    </div>
+
+                </div>
             </div>
         </div>
     )
