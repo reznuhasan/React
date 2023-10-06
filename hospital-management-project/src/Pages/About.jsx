787c4b5a5c1c86303cc020d4446481b2e4aea5c1
reznuhasan <reznuhasan31@gmail.com>
2023-10-07 03:32:42 +0600

    box part done

--- hospital-management-project/src/Pages/About.jsx
+++ hospital-management-project/src/Pages/About.jsx
@@ -3,6 +3,11 @@ import Styles from "../Styles/About.module.css"
 import aboutus from "../assets/aboutus.png"
 import BorderBox from '../components/BorderBox'
 import mission from "../assets/aboutmission.jpeg"
+import box1 from "../assets/box1.png"
+import box2 from "../assets/box2.png"
+import box3 from "../assets/box3.png"
+import box4 from "../assets/box4.png"
+import box5 from "../assets/box5.png"
 const About = () => {
     return (
         <div>
@@ -39,6 +44,64 @@ const About = () => {
                 </div>
                 <div className={Styles.missionImage}>
                     <img src={mission} alt="" />
+                </div>
+            </div>
+            {/* item box part */}
+            <div className={Styles.itemBox}>
+                <div className={Styles.itemBoxValue}>
+                    <h1>Value</h1>
+                    <p>We are committed to providing best-in-class, accessible, private healthcare for all and we encourage all our employees and caregivers to share our values:</p>
+                </div>
+                <div className={Styles.box} style={{ "backgroundColor": "#161F37" }} >
+                    <img src={box1} alt="" />
+                    <div className={Styles.boxContent} >
+                        <h1>Quality</h1>
+                        <p>
+                            We are committed to providing quality healthcare for every patient.
+                        </p>
+                    </div>
+                </div>
+                <div className={Styles.box} style={{ "backgroundColor": "#7C6598" }} >
+                    <img src={box2} alt="" />
+                    <div className={Styles.boxContent} >
+                        <h1>Integrity</h1>
+                        <p>
+                            We do the right thing, every time.
+
+                        </p>
+                    </div>
+                </div>
+                <div className={Styles.box} style={{ "backgroundColor": "#C9214C" }} >
+                    <img src={box3} alt="" />
+                    <div className={Styles.boxContent} >
+                        <h1>Passion</h1>
+                        <p>
+                            We are passionate abouthealthcare and this shows in the care we provide.
+
+                        </p>
+                    </div>
+                </div>
+                <div className={Styles.box} style={{ "backgroundColor": "#9DC161" }} >
+                    <img src={box4} alt="" />
+                    <div className={Styles.boxContent} >
+                        <h1>Respect</h1>
+                        <p>
+                            We are respectful of everyone regardless of our differences and diversity.
+
+                        </p>
+                    </div>
+                </div>
+                <div className={Styles.box} style={{ "backgroundColor": "#71BEDF" }} >
+                    <img src={box5} alt="" />
+                    <div className={Styles.boxContent} >
+                        <h1>
+                            Innovative
+                        </h1>
+                        <p>
+                            We believe innovation allows us to improve our patients’ experience, increase caregiver engagement and ensure the health of our business.
+
+                        </p>
+                    </div>
                 </div>
             </div>
 
